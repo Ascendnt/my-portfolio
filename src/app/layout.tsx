02adc4bd95205cc01f8e56e002f6be92ea71a429
Kenneth Balantucas <kennethbalantucas14@gmail.com>
2024-09-06 17:26:18 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import NavBar from "@/components/NavBar";
+import NavBar from "../components/NavBar";
 
 const montserrat = Montserrat({
   weight: "500",
